dfb541a812958bb7d73c3519c9d47757accfd70c
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-30 16:27:08 +0530

    Shell - installing jenkins script

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,50 +1,61 @@
+#!/bin/bash
+
 USERID=$(id -u)
+DATE=$(date +%F)
+LOG="jenkins-install-${DATE}.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-if [ $USERID -ne 0 ] ; then
-  echo "you need to be a root user to execute"
-  exit 1
+func_validate(){
+if [ $1 -ne 0 ]; then
+	echo -e "$2 ... ${R} FAILURE ${N}" 2>&1 | tee -a $LOG
+# here 2 is STDERR and 1 is STDOUT
+#The tee command, used with a pipe, reads standard input, then writes the output of a program to standard output and simultaneously copies it into the specified file or files.
+	exit 1
+else
+	echo -e "$2 ... ${G} SUCCESS ${N}" 2>&1 | tee -a $LOG
 fi
-
-func_validate () {
-if [ $1 -ne 0 ] ; then
-    echo " $2 ..... failure "
-    exit 1
-  else
-    echo "$2 ...... Success"
-    exit 1
-  fi
-
 }
 
+if [ $USERID -ne 0 ]; then
+	echo -e "${R} You need to be root user to execute this script ${N}"
+	exit 1
+fi
 
-yum update -y
+yum update –y &>>$LOG
+# >> is to append the error log
 
-func_validate $? "updating yum"
-
-dnf install java-11-openjdk-devel
-
-func_validate $? " install java package "
+func_validate $? "Updating YUM"
 
 wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo &>>$LOG
 
-func_validate $? " adding jenkins repo "
+func_validate $? "Adding Jenkins Repo"
 
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key &>>$LOG
 
-func_validate $? "upgrading yum"
+func_validate $? "Import Jenkin key"
 
-yum upgrade -y
+yum upgrade -y &>>$LOG
 
-func_validate $? " installing jenkins "
+func_validate $? "Upgrade YUM"
 
-yum install jenkins -y
+amazon-linux-extras install java-openjdk11 -y &>>$LOG
 
-func_validate $? " enabling jenkins service "
+func_validate $? "Installing OpenJDK 11"
 
-systemctl enable jenkins
+yum install jenkins -y &>>$LOG
 
-func_validate $? " start jenkins service "
+func_validate $? "Installing Jenkins"
 
-systemctl start jenkins
+systemctl enable jenkins &>>$LOG
+
+func_validate $? "Enable Jenkins"
+
+systemctl start jenkins &>>$LOG
+
+func_validate $? "Starting Jenkins"
+
+
 
