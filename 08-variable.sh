8d6327852a56a859b341d2b38ebd48f45a3c71c5
sandeepnaidu <nainala_sandeep@yahoo.com>
2024-04-29 22:25:37 +0530

    Shell - variable

---
+++ 08-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username"
+
+read -s USERNAME   #here USERNAME is a variable
+
+echo "Please enter password"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, password is: $PASSWORD"
