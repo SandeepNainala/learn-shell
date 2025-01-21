2f3427e2b5cf0e6153857eba6db496c3b1211ee4
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-21 17:00:53 +0530

    Shell - packages install scripts

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -4,6 +4,20 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then
+    echo -e"$2 .... $R FAILURE $N"
+    exit 1
+  else
+    echo -e "$2 ... $G SUCCESS $N"
+  fi
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -19,8 +33,9 @@ do
   dnf list installed $i &>>$LOGFILE
   if [ $? -ne 0]
   then
-    echo "$i already installed ... SKIPPING"
+    echo -e "$i already installed ... $Y SKIPPING $N "
   else
-    echo "$i not installed ... need to install "
+    dnf install $i -y &>>LOGFILE
+    VALIDATE $? " Installation of $i "
   fi
 done
