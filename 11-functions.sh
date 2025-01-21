908970d5390521098b973c9e4bd38fedc50c8bb1
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-21 13:11:23 +0530

    Shell - functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S )
-SCRIPT_NAME=$($0 | cut -d "." -f1)     # here $0 is the script name(11-fucntions.sh) we are deleting .sh using cut command
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)     # here $0 is the script name(11-fucntions.sh) we are deleting .sh using cut command
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
