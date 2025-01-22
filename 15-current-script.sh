93deadc974a556ae50f3f12615bc5c47831686b9
sandeepanainala <nainala_sandeep@yahoo.com>
2025-01-22 16:39:43 +0530

    Shell - script for calling one in another script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ COURSE="DEVOPS from current script"
 echo "Before calling other script, Course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-#./16-other-script.sh
+./16-other-script.sh
 
 echo "After Calling other script, course: $COURSE"
---
+++ 16-other-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COURSE="DEVOPS from other script"
+
+echo "Variable value of other script: $COURSE"
+echo "Process id of other script: $$"
+
